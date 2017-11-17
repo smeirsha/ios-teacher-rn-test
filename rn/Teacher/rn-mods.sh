0f039c516e49ecf374bc763003432bb25487a079
Layne Moseley <lmoseley@instructure.com>
2017-11-17 09:28:58 -0700

    November 2017 Update 1

---
+++ rn/Teacher/rn-mods.sh
@@ -0,0 +1,13 @@
+# see https://github.com/facebook/react-native/issues/10865#issuecomment-302661876
+# and here https://github.com/facebook/react-native/pull/10941
+# Apparently this is a hot debate in the rn community :)
+sed -i '' '/RCTLogError(@"Setting onMessage on a WebView overrides existing values of window.postMessage, but a previous value was defined");/d' node_modules/react-native/React/Views/RCTWebView.m
+
+sed -i '' 's/RCTAnimation/React/' node_modules/react-native/Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.h
+
+# https://github.com/facebook/react-native/issues/15808
+sed -i '' -e 's/MAX(-contentInset.top, MIN(contentSize.width/MAX(-contentInset.left, MIN(contentSize.width/' node_modules/react-native/React/Views/RCTScrollView.m
+sed -i '' -e 's/MAX(-contentInset.left, MIN(contentSize.height/MAX(-contentInset.top, MIN(contentSize.height/' node_modules/react-native/React/Views/RCTScrollView.m
+
+# https://github.com/facebook/react-native/issues/16039
+sed -i '' 's#<fishhook/fishhook.h>#\"fishhook.h\"#g' ./node_modules/react-native/Libraries/WebSocket/RCTReconnectingWebSocket.m
